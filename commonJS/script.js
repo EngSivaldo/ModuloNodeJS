48f033a1ee912b8d1195728eb42827b54f8ff622
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-11-19 11:53:15 -0300

    modo de exportar e importar modulos com commonJS

---
+++ commonJS/script.js
@@ -0,0 +1,19 @@
+console.log('Olá, boa noite!Pasta commonJS');
+
+
+function addFiveNumbers(number1, number2, number3, number4, number5) {
+  return number1+number2+number3+number4+number5;
+}
+
+function selectBestCountry() {
+  return 'Brasil';
+}
+
+// console.log(global)
+
+global.console.log('Ola,feliz ano novo!');
+
+
+//modo de importar commonJS(padrão)
+const script3 = require('./script3');
+console.log(script3.BestCountry());
---
+++ commonJS/script3.js
@@ -0,0 +1,12 @@
+function selectBestCountry() {
+  return 'Argentina';
+}
+
+//como  exportar
+module.exports.BestCountry = selectBestCountry;
+
+// console.log(module);
+
+
+//commonJS  -> No back-end(padrão)
+//ESModules -> No front-end e no back-end
