b8e5275dcc38d0e3149586f26e81b785ad69238f
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-11-19 18:57:31 -0300

    Bliblioteca O módulo FS

---
+++ fileSystem/app.mjs
@@ -0,0 +1,16 @@
+import { readFile } from 'node:fs/promises';
+
+
+async function readTextFile(filePath) {
+  try {
+    const recipe = await readFile(filePath, {encoding: 'utf-8'});
+    console.log(recipe);
+    
+  } catch (error) {
+    console.error('Erro na leitura do arquivo!');
+    
+  }
+  
+}
+
+readTextFile('./receita.txt');
